eae53554a491ccda57be50fda3fbd79674d77dcb
Shaz Alam <99272333+Shaz999@users.noreply.github.com>
2024-04-25 22:17:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    vector<int> find(int arr[], int n , int x )
+    {
+         vector<int> ans(2, -1);
+        
+        // Find the first occurrence of x
+        int left = 0, right = n - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == x)
+            {
+                ans[0] = mid;
+                right = mid - 1; 
+            }
+            else if (arr[mid] < x)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+    
+        // Find the last occurrence of x
+        left = 0;
+        right = n - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == x)
+            {
+                ans[1] = mid;
+                left = mid + 1; 
+            }
+            else if (arr[mid] < x)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
