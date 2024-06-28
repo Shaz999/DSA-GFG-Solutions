f9e41d01aaf6cafc684d1248fd1c540d0752e52d
Shaz Alam <99272333+Shaz999@users.noreply.github.com>
2024-06-28 12:47:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count the triplets/count-the-triplets.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countTriplet(int arr[], int n)
+	{
+	    int count =0;
+	    sort(arr,arr+n);
+	    for(int i=0;i<n-1;i++){
+	        for(int j=i+1;j<n;j++){
+	        int sum =arr[i]+arr[j];
+	        if(binary_search(arr,arr+n,sum)){
+	            count++;
+	        }
+	    }}
+	    return count;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        	cin >> arr[i];
+
+        Solution ob;
+        cout << ob.countTriplet(arr, n) << "\n";
+
+    }
+
+    return 0;
+}
+// } Driver Code Ends
